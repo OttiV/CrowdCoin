7fc0201206e840ef5c05f7642aa350244c245750
Vgnttv <o.vignani@gmail.com>
2022-08-10 16:32:25 +0200

    Display single Campaign information

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import getCampaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = await getCampaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
+  renderCards() {
+    const {
+      minimumContribution,
+      balance,
+      requestCount,
+      approversCount,
+      manager,
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of manager',
+        description:
+          'The manager created this campaign and can create requests to withdraw money',
+        style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description:
+          'You must contribute at least this much wei to become an approver',
+        style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: requestCount,
+        meta: 'Number of requests',
+        description:
+          'A request tries to withdraw monty from the Contract. Requests must be approved by approvers',
+        style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: approversCount,
+        meta: 'Number of approvers',
+        description:
+          'Number of people who have already donated to this campaign',
+        style: { overflowWrap: 'break-word' },
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether',
+        description:
+          'The balance is how much money the Campaign has left to spend',
+        style: { overflowWrap: 'break-word' },
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
-        <h3>Show</h3>
+        <h3>Campaign Details</h3>
+        {this.renderCards()}
       </Layout>
     );
   }

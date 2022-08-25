a632c974e19b5d0f3bc08f69d9009fe34a305e93
Vgnttv <o.vignani@gmail.com>
2022-08-25 14:33:18 +0200

    Refactor Components folder

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,10 @@ import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import Header from './Header';
 
-
-const Layout = (props) => (
+const Layout = ({ children }) => (
   <Container>
     <Header />
-    {props.children}
+    {children}
   </Container>
 );
 
